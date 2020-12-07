4226ab7048aec5618b8038037f40e925bb4c5f8b
Elizabeth Stansberry <estansberry20@stuy.edu>
2020-12-07 16:04:19 -0700

    Finished code

--- Range.java
+++ Range.java
@@ -1,31 +1,31 @@
 import java.util.NoSuchElementException;
 public class Range implements IntegerSequence{
   private int start, end, current;
-  public Range(int start,  int end){
-    start = start;
-    end = end;
-
+  public Range(int a,  int b){
+    start = a;
+    end = b;
+    current = a;
   }
   public void reset(){
     current = start;
   }
   public int length(){
-    return(end - start);
+    return(end - start + 1);
   }
   public boolean hasNext(){
-    return(current < end && current > start);
+    return(current <= end && current >= start);
   }
-
 
   public int next(){
     if(hasNext() == false){
       throw new NoSuchElementException("Error: this is the last element in this range. There is no next element.");
     }else{
       current = current + 1;
-      return(current);
+      return(current - 1);
     }
 
   }
-  public static void main(String[] args){}
+  public static void main(String[] args){
+  }
 
 }
