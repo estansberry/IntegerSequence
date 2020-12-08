cd8cf4a40e092b4082436de49acf3eaa41c4346b
Elizabeth Stansberry <estansberry20@stuy.edu>
2020-12-08 10:38:20 -0700

    Added methods

--- ArraySequence.java
+++ ArraySequence.java
@@ -1,3 +1,31 @@
+import java.util.NoSuchElementException;
 public class ArraySequence implements IntegerSequence{
+  private int currentIndex;
+  private int[] data;
+
+  public ArraySequence(int[] other){
+    currentIndex = 0;
+    data = other;
+  }
+
+  public int length(){
+    return(data.length);
+  }
+
+  public void reset(){
+    currentIndex = 0;
+  }
+
+  public boolean hasNext(){
+    return(currentIndex > length());
+  }
+  public int next(){
+    if(hasNext() == false){
+      throw new NoSuchElementException("Error: this is the last element in this Sequence. There is no next element.");
+    }else{
+      currentIndex = currentIndex + 1;
+      return(data[currentIndex - 1]);
+    }
+  }
 
 }
